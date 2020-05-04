548f2280e54787f9d76751a60d6dbf5f4bae6142
edinalami <edinalami@gmail.com>
2020-05-04 16:41:09 +0200

    edit gitignore

---
+++ build/precache-manifest.302c15fc063c09936f25ed583801a5ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a179f821f2aa854a0846c993caa9fd9c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9c584d4873c3c0bd1ed2",
+    "url": "./static/css/main.ce140f75.chunk.css"
+  },
+  {
+    "revision": "f9f1c85ed2dfb059483d",
+    "url": "./static/js/2.db39bea0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.db39bea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c584d4873c3c0bd1ed2",
+    "url": "./static/js/main.079c6789.chunk.js"
+  },
+  {
+    "revision": "c999e1aafeacb3931724",
+    "url": "./static/js/runtime-main.9edb4eab.js"
+  }
+]);
